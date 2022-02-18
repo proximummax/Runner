39e2d6e39bd390027a507a80987b9b3c4f37c458
proximummax <proximum890@gmail.com>
2022-02-18 15:41:09 +0700

    Init

---
+++ Assets/Scripts/Enemy/EnemyMover.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    private void Update()
+    {
+        transform.Translate(Vector3.left * _speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : ObjectPool
+{
+    [SerializeField] private GameObject _enemyPrefab;
+    [SerializeField] private Transform[] _spawnPoints;
+    [SerializeField] private float _secondsBetweenSpawn;
+
+    private float _elapsedTime = 0;
+
+    private void Start()
+    {
+        Initialize(_enemyPrefab);
+    }
+    private void Update()
+    {
+        _elapsedTime += Time.deltaTime;
+
+        if (_elapsedTime >= _secondsBetweenSpawn)
+        {
+            if (TryGetObject(out GameObject enemy))
+            {
+                _elapsedTime = 0;
+
+                int spawnPointNumber = Random.Range(0, _spawnPoints.Length);
+
+                SetEnemy(enemy, _spawnPoints[spawnPointNumber].position);
+            }
+        }
+    }
+    private void SetEnemy(GameObject enemy, Vector3 spawnPoint)
+    {
+        enemy.SetActive(true);
+        enemy.transform.position = spawnPoint;
+    }
+}
